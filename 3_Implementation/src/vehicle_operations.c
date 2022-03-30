f5a39dd986187cdea4efd9dcc3654543571bb839
JanhviKohade <90107175+JanhviKohade@users.noreply.github.com>
2022-03-31 01:14:16 +0530

    Add files via upload

---
+++ 3_Implementation/src/vehicle_operations.c
@@ -0,0 +1,31 @@
+#include<windows.h>
+
+struct vehicle * add ( int operand1, int operand2, int operand3, int operand4 )
+{
+
+}
+
+void changecol ( struct operand*) 
+{
+
+}
+
+void getfreerowcol ( int operand1, int operand*)
+{
+
+}
+
+void getrcbyinfo ( int operand1, int operand2, int operand* )
+{
+
+}
+
+int Display(int operand1, int operand2)
+{
+
+}
+
+void del ( struct operand* ) 
+{
+    
+}
